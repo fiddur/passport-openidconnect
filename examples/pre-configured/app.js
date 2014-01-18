7014f2f904f5868a35f43d7170880608628f2fa2
igi64 <igi64@yahoo.com>
2014-01-18 13:46:32 +0100

    pre-configured example

--- examples/pre-configured/app.js
+++ examples/pre-configured/app.js
@@ -7,20 +7,21 @@ var http = require('http');
 var path = require('path');
 var passport = require('passport');
 // npm passport-openidconnect package is not up to date
-var MitreIDStrategy = require('passport-openidconnect').Strategy;
+var OpenidConnectStrategy = require('passport-openidconnect').Strategy;
 
-var DOMAIN_NAME = '--insert-your-domain-name-here--';
+var RP_DOMAIN_NAME = 'localhost.airydrive.org'; //'--insert-your-relying-party-domain-name-here--'
+var OP_DOMAIN_NAME = 'mitreid.org'; //'--insert-your-openid-provider-domain-name-here--'
 
-var SCOPES = 'profile email';
-// register a new client at https://mitreid.org/manage/dev/dynreg
-var MITREID_CLIENT_ID = '--insert-mitreid-client-id-here--';
-var MITREID_CLIENT_SECRET = '--insert-mitreid-client-secret-here--';
+var SCOPE = 'profile email';
+// register a new client at e.g. https://mitreid.org/manage/dev/dynreg
+var CLIENT_ID = 'abf911b1-47ba-4163-881d-2b995cba8a1d';//'--insert-oidc-client-id-here--';
+var CLIENT_SECRET = 'AI0R7dSkCt5ye30lKeMIS35whbIOMYASLvwd-nZeVW7sLJSchpI1z7q_UNugyXTFkTl4AKWdqiwykje_y-msXJA';//'--insert-oidc-client-secret-here--';
 // your callback url
-var CALLBACK_URL = 'http://' + DOMAIN_NAME + '/auth/mitreid/callback';
-// from https://mitreid.org/.well-known/openid-configuration/
-var AUTHORIZATION_URL = 'https://mitreid.org/authorize';
-var TOKEN_URL = 'https://mitreid.org/token';
-var USER_INFO_URL = 'https://mitreid.org/token';
+var CALLBACK_URL = 'http://' + RP_DOMAIN_NAME + '/auth/oidc/callback';
+// e.g. from https://mitreid.org/.well-known/openid-configuration/
+var AUTHORIZATION_URL = 'https://' + OP_DOMAIN_NAME + '/authorize';
+var TOKEN_URL = 'https://' + OP_DOMAIN_NAME + '/token';
+var USER_INFO_URL = 'https://' + OP_DOMAIN_NAME + '/userinfo';
 
 passport.serializeUser(function(user, done) {
     done(null, user);
@@ -30,21 +31,18 @@ passport.deserializeUser(function(obj, done) {
     done(null, obj);
 });
 
-// Use the MitreIDStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an Issuer, User ID, User profile,
-//   accessToken and refreshToken), and invoke a callback with a user object.
-passport.use(new MitreIDStrategy({
-        scope: SCOPES,
-        clientID: MITREID_CLIENT_ID,
-        clientSecret: MITREID_CLIENT_SECRET,
-        callbackURL: CALLBACK_URL,
-        authorizationURL: AUTHORIZATION_URL,
-        tokenURL: TOKEN_URL,
-        userInfoURL: USER_INFO_URL},
+passport.use(new OpenidConnectStrategy({
+  scope: SCOPE,
+  clientID: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  callbackURL: CALLBACK_URL,
+  authorizationURL: AUTHORIZATION_URL,
+  tokenURL: TOKEN_URL,
+  userInfoURL: USER_INFO_URL
+},
     function(iss, sub, profile, accessToken, refreshToken, done) {
         process.nextTick(function () {
-            return done(null, user);
+            return done(null, profile);
         });
     }
 ));
@@ -86,25 +84,15 @@ app.get('/login', function(req, res){
     res.render('login', { user: req.user });
 });
 
-// GET /auth/mitreid
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in MitreID authentication will involve
-//   redirecting the user to mitreid.org.  After authorization, MitreID
-//   will redirect the user back to this application at /auth/mitreid/callback
-app.get('/auth/mitreid',
-    passport.authenticate('openidconnect', { failureRedirect: '/login' }),
-    function(req, res){
-        // The request will be redirected to MitreID for authentication, so this
+app.get('/auth/oidc/login', passport.authenticate('openidconnect',
+  {callbackURL: '/auth/oidc/callback', failureRedirect: '/login'}),
+  function(req, res){
+        // The request will be redirected to OP for authentication, so this
         // function will not be called.
 });
 
-// GET /auth/mitrid/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
-app.get('/auth/mitreid/callback',
-    passport.authenticate('mitreid', { failureRedirect: '/login' }),
+app.get('/auth/oidc/callback',
+    passport.authenticate('openidconnect', { failureRedirect: '/login' }),
     function(req, res) {
         // Successful authentication, redirect home.
         res.redirect('/');
